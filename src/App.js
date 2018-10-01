270888eaf4914d27a36ccea563007f015c9f8d3b
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-10-01 13:24:35 +0300

    Add book list class component and book detail functional component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import BookList from './components/book-list';
+import BookDetail from './components/book-detail';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 39 },
+        { title: 'The Dark Tower', pages: 85 },
+        { title: 'Eloquent Ruby', pages: 1 }
+      ],
+      activeBook: null
+    };
+  }
+
+  selectBook = (book) => {
+    this.setState({ activeBook: book });    
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +31,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BookList books={this.state.books} selectBook={this.selectBook} />
+        <BookDetail book={this.state.activeBook} />
       </div>
     );
   }
---
+++ src/components/book-list.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li
+          onClick={() => this.props.selectBook(book)}
+          key={book.title}
+          className="list-group-item"
+        >
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+export default BookList;
