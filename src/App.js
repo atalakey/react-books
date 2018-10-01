f4a6f92830d72e0946b011874fed8754520f9e07
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-10-01 13:32:14 +0300

    Convert book list component into a container component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import BookList from './components/book-list';
-import BookDetail from './components/book-detail';
+import BookList from './containers/book-list';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      books: [
-        { title: 'Javascript: The Good Parts', pages: 101 },
-        { title: 'Harry Potter', pages: 39 },
-        { title: 'The Dark Tower', pages: 85 },
-        { title: 'Eloquent Ruby', pages: 1 }
-      ],
-      activeBook: null
-    };
-  }
-
-  selectBook = (book) => {
-    this.setState({ activeBook: book });    
-  }
-
   render() {
     return (
       <div className="App">
@@ -31,8 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <BookList books={this.state.books} selectBook={this.selectBook} />
-        <BookDetail book={this.state.activeBook} />
+        <BookList />
       </div>
     );
   }
