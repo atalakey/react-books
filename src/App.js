99bb8a71fd53f0026c8e914425d8e0c7915a1244
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-10-01 13:39:23 +0300

    Convert book detail component into a container component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import BookList from './containers/book-list';
+import BookDetail from './containers/book-detail';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
